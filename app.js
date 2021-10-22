d9d8dd7b220b017e3f17abe63ea1c957477d8ea9
Lestrous <shtol.leonid@mail.ru>
2021-10-22 18:35:34 +0500

    week6

---
+++ app.js
@@ -0,0 +1,45 @@
+export default (express, bodyParser, createReadStream, crypto, http) => {
+    const app = express();
+
+    const CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type,Accept,Access-Control-Allow-Headers'
+    };
+
+    app
+        .use((r, res, next) => { r.res.set(CORS); next(); })
+        .use(bodyParser.urlencoded({ extended: true }))
+        .use(bodyParser.json())
+
+        .all('/login/', (req, res) => res
+            .send('day108')
+        )
+        .all('/code/', (req, res) => {
+            const fileSrc = import.meta.url.substring(import.meta.url.length - 6);
+
+            res.set({'Content-Type': 'text/plain; charset=UTF-8'});
+            createReadStream(fileSrc).pipe(res);
+        })
+        .all('/sha1/:input/', (req, res) => {
+            const hash = crypto.createHash('sha1');
+            hash.update(req.params.input);
+
+            res.send( hash.digest('hex') );
+        })
+        .all('/req/', (req, res) => {
+            const addr = req.method === 'POST' ? req.body.addr : req.query.addr;
+
+            http.get(addr, (httpRes, str = '') => {
+                httpRes
+                    .on('data', data => str += data)
+                    .on('end', () => res.send(str));
+            });
+        })
+        .all('*', (req, res) => res
+            .set({'X-Author': 'day108'})
+            .send('day108')
+        );
+
+    return app;
+}
--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createReadStream } from 'fs';
+import crypto from 'crypto';
+import http from 'http';
 
-const app = express();
+import appSrc from './app.js';
 
-const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
-};
+const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
-app
-.use((r, res, next) => { r.res.set(CORS); next(); })
-.all('/', (req, res) => res
-    .set({'X-Author': 'day108'})
-    .send('day108')
-)
-.all('/login/', (req, res) => res
-    .set({'X-Author': 'day108', 'Content-Type': 'text/plain; charset=UTF-8'})
-    .send('day108')
-)
-.all('/sample/', (req, res) => res
-    .set({'X-Author': 'day108', 'Content-Type': 'text/plain; charset=UTF-8'})
-    .send('function task(x) { return x * this ** 2; }')
-)
-.all('/result4/', (req, res) => {
-    let requestBody = '';
-
-    req.on('data', chunk => {
-        requestBody += chunk;
-    });
-
-    req.on('end', () => res
-        .status(200)
-        .set({'Content-Type': 'application/json; charset=UTF-8'})
-        .json({"message": "day108", "x-result": req['headers']['x-test'], "x-body": requestBody})
-    );
-})
-.all('/nikita/', (req, res) => res
-    .send('Здарова. Завтра вынесу тебя')
-)
-.listen(process.env.PORT ?? 4321);
+app.listen(process.env.PORT ?? 4321);
