9fbf1d33c2d79df619d2ea838be2e40ab34a16de
Lestrous <shtol.leonid@mail.ru>
2021-10-22 19:15:29 +0500

    week6 fixes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
 
     const CORS = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE'
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type,Accept,Access-Control-Allow-Headers'
     };
 
     app
